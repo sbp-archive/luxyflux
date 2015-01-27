ffb5d4f3c9458b0188d77f9a257a648f8d65c9e2
Tommy Maintz <tmaintz@schubergphilis.com>
2015-01-28 00:33:45 +0100

    Initial version of LuxaFlux.
    No good test coverage yet, nor a fully implemented Store

--- src/connect-flux.js
+++
@@ -1,1 +0,0 @@
-window.angular.module('connect-flux', []);
--- src/store.js
+++ src/store.js
@@ -1,18 +1,73 @@
-(function (angular) {
-    'use strict';
+var _storeCounter = 1;
 
-    var Store = function (options) {
-    };
+export class Store {
+    constructor(
+        name,
+        dispatcher,
+        handlers = {},
+        initializeFn = ()=>{}
+    ) {
+        if (!dispatcher) {
+            throw new Error('You have to specify a dispatcher when creating a Store');
+        }
 
-    Store.prototype = {
-        addChangeListener: function() {},
-        setState: function() {},
-        hasChanged: function() {},
-        dispose: function() {},
-        handleAction: function() {}
-    };
+        this.name = name;
+        this.dispatcher = dispatcher;
+        this.initialize = initializeFn.bind(this);
 
-    angular.module('connect-flux').service('FluxStore', [function () {
-        return Store;
-    }]);
-}(window.angular));
+        this.addActionHandlers(handlers);
+        this.initialize();
+    }
+
+    set dispatcher(dispatcher) {
+        if (this._dispatcher !== dispatcher) {
+            this._dispatcher.unregister(this.name);
+        }
+        dispatcher.register(this.name, this.callback);
+        this._dispatcher = dispatcher;
+    }
+
+    get dispatcher() {
+        return this._dispatcher;
+    }
+
+    set name(name) {
+        if (!name) {
+            name = `luxaflux-store-name-${_storeCounter++}`;
+        }
+        this._name = name;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    /**
+     * Bind callbacks to specific actions
+     *
+     * @param {Object} callbacks An object where each key is the name of an action
+     * and the value is the callback to be called when that action is dispatched
+     */
+    addActionHandlers(actionHandlers) {
+
+    }
+
+    /**
+     * This is the callback method that needs to be registered with the dispatcher
+     */
+    callback(action, payload) {
+
+    }
+
+    /**
+     * Creates a promise and waits for the callbacks specified to complete before resolve it.
+     *
+     * @param  {Function<Array>|Function} callbacks The callbacks to wait for.
+     * @return {Promise} A promise to be resolved when the specified callbacks are completed.
+     */
+    waitFor(tokens) {
+
+    }
+}
+
+export default Store;
